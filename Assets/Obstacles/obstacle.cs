938f0c17a3f95d344b127a6a975cfad8f831dd92
Tasman Smith <tasmanvs@gmail.com>
2025-08-02 15:02:09 -0700

    Obstacles working! The player must enter the obstacle code when they enter the obstacle, otherwise they get ejected from the obstacle. If they succeed, they make it to the other end.

--- Assets/Obstacles/obstacle.cs
+++ Assets/Obstacles/obstacle.cs
@@ -1,10 +1,20 @@
 using System.ComponentModel.Design;
 using UnityEngine;
+using UnityEngine.InputSystem; // Add this for the new Input System
 
 public class obstacle : MonoBehaviour
 {
     public BoxCollider2D door_a;
     public BoxCollider2D door_b;
+
+    public string obstacleWord = "UDLR"; // The word the player must spell to pass through the obstacle
+
+    private bool awaitingInput = false;
+    private string playerInput = "";
+    private Vector2 originalPlayerPosition;
+    private Collider2D currentPlayerCollider;
+    private BoxCollider2D enteredDoor;
+    private BoxCollider2D targetDoor;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,33 +25,87 @@ public class obstacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (awaitingInput && currentPlayerCollider != null)
+        {
+            // Listen for arrow key input and build the input string using the new Input System
+            if (Keyboard.current.upArrowKey.wasPressedThisFrame) playerInput += "U";
+            if (Keyboard.current.downArrowKey.wasPressedThisFrame) playerInput += "D";
+            if (Keyboard.current.leftArrowKey.wasPressedThisFrame) playerInput += "L";
+            if (Keyboard.current.rightArrowKey.wasPressedThisFrame) playerInput += "R";
+
+            // Check for immediate failure
+            int len = playerInput.Length;
+            if (len > 0 && len <= obstacleWord.Length)
+            {
+                if (playerInput[len - 1] != obstacleWord[len - 1])
+                {
+                    // Incorrect input at this step: fail immediately
+                    player playerScript = currentPlayerCollider.GetComponent<player>();
+                    if (playerScript != null)
+                    {
+                        playerScript.ReverseLastMove();
+                    }
+                    Debug.Log("Incorrect! Returned to previous position.");
+                    // Reset state
+                    awaitingInput = false;
+                    playerInput = "";
+                    currentPlayerCollider = null;
+                    enteredDoor = null;
+                    targetDoor = null;
+                    player.isFrozen = false;
+                    return;
+                }
+            }
+
+            // If input is complete and correct, teleport
+            if (playerInput.Length == obstacleWord.Length)
+            {
+                currentPlayerCollider.transform.position = targetDoor.transform.position;
+                Debug.Log("Correct! Teleported to the other door.");
+                // Reset state
+                awaitingInput = false;
+                playerInput = "";
+                currentPlayerCollider = null;
+                enteredDoor = null;
+                targetDoor = null;
+                player.isFrozen = false;
+            }
+        }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D player_collider)
     {
-        // Print debug message about other.
-        Debug.Log("Obstacle triggered by: " + other.name);
+        // Print debug message about player_collider.
+        Debug.Log("Obstacle triggered by: " + player_collider.name);
         // Print the tag
-        Debug.Log("Obstacle triggered by tag: " + other.tag);
+        Debug.Log("Obstacle triggered by tag: " + player_collider.tag);
         // Check if the collider belongs to the player
-        if (other.CompareTag("Player"))
+        if (player_collider.CompareTag("Player"))
         {
-            Vector3 playerPos = other.transform.position;
+            Vector2 playerPos = player_collider.transform.position;
             // Check which door the player is overlapping
+            // And move the xy position to the other door's position
             if (door_a.OverlapPoint(playerPos))
             {
-                // other.transform.position = door_b.transform.position;
-                // Just move the xy position to the other door's position
-                other.transform.position = new Vector3(door_b.transform.position.x, door_b.transform.position.y, playerPos.z);
-                // Debug message to confirm the teleportation
+                enteredDoor = door_a;
+                targetDoor = door_b;
             }
             else if (door_b.OverlapPoint(playerPos))
             {
-                // other.transform.position = door_a.transform.position;
-                // Just move the xy position to the other door's position
-                other.transform.position = new Vector3(door_a.transform.position.x, door_a.transform.position.y, playerPos.z);
+                enteredDoor = door_b;
+                targetDoor = door_a;
             }
+            else
+            {
+                return;
+            }
+
+            // No need to store previousPosition from player script anymore
+            currentPlayerCollider = player_collider;
+            awaitingInput = true;
+            playerInput = "";
+            player.isFrozen = true; // Freeze player while awaiting input
+            Debug.Log("Enter the obstacle word using arrow keys: " + obstacleWord);
         }
     }
 }
