27a00f4d678af637b2580a3b5cf43416e511770e
Tasman Smith <tasmanvs@gmail.com>
2025-08-01 17:09:54 -0700

    s

--- Assets/Player/player.cs
+++ Assets/Player/player.cs
@@ -5,10 +5,18 @@ public class player : MonoBehaviour
 {
     public float moveAmount = 1f;
 
+    // Sprites for each direction
+    public Sprite leftSprite;
+    public Sprite rightSprite;
+    public Sprite upSprite;
+    public Sprite downSprite;
+
+    private SpriteRenderer spriteRenderer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -18,10 +26,26 @@ public class player : MonoBehaviour
         var keyboard = Keyboard.current;
         if (keyboard != null)
         {
-            if (keyboard.leftArrowKey.wasPressedThisFrame) move.x -= moveAmount;
-            if (keyboard.rightArrowKey.wasPressedThisFrame) move.x += moveAmount;
-            if (keyboard.upArrowKey.wasPressedThisFrame) move.y += moveAmount;
-            if (keyboard.downArrowKey.wasPressedThisFrame) move.y -= moveAmount;
+            if (keyboard.leftArrowKey.wasPressedThisFrame) {
+                move.x -= moveAmount;
+                if (leftSprite != null && spriteRenderer != null)
+                    spriteRenderer.sprite = leftSprite;
+            }
+            if (keyboard.rightArrowKey.wasPressedThisFrame) {
+                move.x += moveAmount;
+                if (rightSprite != null && spriteRenderer != null)
+                    spriteRenderer.sprite = rightSprite;
+            }
+            if (keyboard.upArrowKey.wasPressedThisFrame) {
+                move.y += moveAmount;
+                if (upSprite != null && spriteRenderer != null)
+                    spriteRenderer.sprite = upSprite;
+            }
+            if (keyboard.downArrowKey.wasPressedThisFrame) {
+                move.y -= moveAmount;
+                if (downSprite != null && spriteRenderer != null)
+                    spriteRenderer.sprite = downSprite;
+            }
         }
         if (move != Vector3.zero)
         {
